71a7c14e8e3db74674422ab98bba60ce72e898a1
Crystal Martin <drcrystalm@gmail.com>
2019-10-18 13:01:32 -0500

    I have some links

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@ import React from "react"
 import Header from "./components/Header.js"
 import WelcomePage from "./components/WelcomePage"
 import CharacterList from "./components/CharacterList"
-import { Route } from "react-router-dom"
+import LocationList from "./components/LocationsList"
+import { BrowserRouter as Router, Route, Link, Switch } from "react-router-dom"
 
 export default function App() {
     return (
-        <div>
-            <Route exact path='./' component={WelcomePage} />
-            <Header />
-            <CharacterList />
-        </div>
+        <Router>
+            <div>
+                <nav ClassName='main-nav'>
+                    <ul>
+                        <li>
+                            <Link to='/'>Home</Link>
+                        </li>
+                        <li>
+                            <Link to='/characters'>Characters</Link>
+                        </li>
+                        <li>
+                            <Link to='/location'>Location</Link>
+                        </li>
+                    </ul>
+                </nav>
+                <Switch>
+                    <Route path='/' exact component={WelcomePage} />
+                    <Route path='/characters' component={CharacterList} />
+                    <Route path='/location' component={LocationList} />
+                </Switch>
+            </div>
+        </Router>
     )
 }
